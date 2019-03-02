ba46f3d5c508f407a98d6d31077268bed6628558
Matthew Faller <matthew.faller@rbc.com>
2019-03-02 11:07:34 -0500

    Added TableViewModel

--- Taskly/Taskly/ViewModels/TaskTableViewModel.swift
+++ Taskly/Taskly/ViewModels/TaskTableViewModel.swift
@@ -11,6 +11,54 @@ import UIKit
 
 internal class TaskTableModel {
  
+    internal init(service: TaskService) {
+        self.service = service
+    }
     
+    private let service: TaskService
+    internal weak var delegate: TaskTableModelDelegate?
+    
+    internal func requestUpdate() {
+        
+        service.getTasks { (serviceResult) in
+            
+            switch serviceResult {
+                
+            case let .success(models):
+                self.handleSuccess(models)
+                
+            case let .failure(error):
+                self.handleFailure(error)
+                
+            }
+        }
+    }
+    
+    private func handleSuccess(_ models: [TaskModel]) {
+        let incomeplete = models.filter(isIncomplete)
+        let completed = models.filter(isCompleted)
+        
+        delegate?.didFinishFetchingTasks(incomplete: incomeplete, completed: completed)
+    }
+    
+    private func handleFailure(_ error: TaskError) {
+        // Additional error processing, as needed.
+        
+        delegate?.didRecieveError(error)
+    }
+    
+    private func isIncomplete(_ model: TaskModel) -> Bool {
+        return !model.completed
+    }
+    
+    private func isCompleted(_ model: TaskModel) -> Bool {
+        return model.completed
+    }
 }
 
+internal protocol TaskTableModelDelegate: class {
+    
+    func didFinishFetchingTasks(incomplete: [TaskModel], completed: [TaskModel])
+    
+    func didRecieveError(_ taskError: TaskError)
+}
